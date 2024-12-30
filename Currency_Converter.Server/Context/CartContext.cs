7b157051696c6c9123e76a2965d5e45a5019f6f4
Evan Holloway <eholloway00@gmail.com>
2024-12-29 21:44:32 -0800

    adds cart controller GET/POST

---
+++ Currency_Converter.Server/Context/CartContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Currency_Converter.Server.Context;
+
+public class CartContext : DbContext
+{
+	public CartContext(DbContextOptions<CartContext> options) : base(options)
+	{
+
+	}
+
+	public DbSet<CartContext> Carts { get; set; }
+}
